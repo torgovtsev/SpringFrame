359cc9eef58faa96f7237b6f961b0dce32e89434
Sergey Torgovtsev <Sergey.Torgovtsev@t-systems.com>
2018-10-04 17:40:23 +0300

    yes, we did it, new configuration. go to bar drink

---
+++ springframe-war/src/main/java/net/tsystems/springframe/configuration/HibernateConfiguration.java
@@ -0,0 +1,11 @@
+package net.tsystems.springframe.configuration;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author storgovt
+ */
+@Configuration
+public class HibernateConfiguration {
+
+}
